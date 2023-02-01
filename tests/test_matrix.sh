502f9f1f3e7c6d99d625ff1afb2b677d40c361cb
Mitchell Berry <foss@mitchellberry.com>
2023-02-01 17:51:52 +1100

    Add: cfg testing

--- tests/test_matrix.sh
+++ tests/test_matrix.sh
@@ -8,17 +8,19 @@ announce(){
   echo -e "\n\n$edge"; echo "$title"; echo "$edge";
 }
 
+# Keep existing RUSTFLAGS
+RUSTFLAGS=${RUSTFLAGS:-""}
+RUSTFLAGS+=" --cfg dilithium_kat"
+
 MODE=("mode2" "mode3" "mode5")
 
 for mode in ${MODE[@]}; do
   announce "Dilithium $mode"
-  cargo test --features "$mode KAT"
+  RUSTFLAGS=$RUSTFLAGS cargo test --features "$mode"
 
   announce "Dilithium $mode AES"
-  cargo test --features "$mode aes KAT"
+  RUSTFLAGS=$RUSTFLAGS cargo test --features "$mode aes"
 
   announce "Dilithium $mode Random Signing"
-  cargo test --features "$mode random_signing"
+  RUSTFLAGS=$RUSTFLAGS cargo test --features "$mode random_signing"
 done
-
-
