a95f2c0df39514b937c42745c785f65e23c441b2
Mitchell Berry <foss@mitchellberry.com>
2023-02-02 07:16:09 +1100

    Add: Benchmarking

---
+++ benches/api.rs
@@ -0,0 +1,24 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use pqc_dilithium::*;
+
+fn sign_small_msg(c: &mut Criterion)
+{
+  let keys = Keypair::generate();
+  let msg = "Hello".as_bytes();
+  c.bench_function("Sign Small Message", |b| {
+    b.iter(|| keys.sign(black_box(msg)))
+  });
+}
+
+fn verify_small_msg(c: &mut Criterion)
+{
+  let keys = Keypair::generate();
+  let msg = "Hello".as_bytes();
+  let sig = keys.sign(msg);
+  c.bench_function("Verify Small Message", |b| {
+    b.iter(|| verify(black_box(sig), black_box(msg), black_box(&keys.public)))
+  });
+}
+
+criterion_group!(benches, sign_small_msg, verify_small_msg);
+criterion_main!(benches);
